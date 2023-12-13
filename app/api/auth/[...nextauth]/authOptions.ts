1342114a73c5c8f6a59029505a39e73ff96c2ca7
Kevin Minh <56735050+Kevinminh@users.noreply.github.com>
2023-12-13 17:37:24 +0100

    removed github provider from authoptions

--- app/api/auth/[...nextauth]/authOptions.ts
+++ app/api/auth/[...nextauth]/authOptions.ts
@@ -22,10 +22,6 @@ export const authOptions: NextAuthOptions = {
 		signIn: `/sign-in`,
 	},
 	providers: [
-		GitHubProvider({
-			clientId: process.env.GITHUB_CLIENT_ID!,
-			clientSecret: process.env.GITHUB_CLIENT_SECRET!,
-		}),
 		GoogleProvider({
 			clientId: process.env.GOOGLE_CLIENT_ID!,
 			clientSecret: process.env.GOOGLE_SECRET!,
